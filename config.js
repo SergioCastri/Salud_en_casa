c81a51fb4cf09b4fcbc78e3dc293ba66bac68c35
sergiocastri <scastrillon347@gmail.com>
2018-04-05 18:24:19 -0500

    Ready to heroku

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 const rootPath = path.resolve(__dirname, '.')
-const port = process.env.Dev ? 8080 : 3000
+const port = process.env.Dev || 8080
 const morganMode = process.env.Dev ? 'dev' : 'tiny'
 
 module.exports = {
